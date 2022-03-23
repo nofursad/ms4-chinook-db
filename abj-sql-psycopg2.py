a507c109b3094edf803f08d2b73f145f701d94c7
Abhisek Joshi <nofursad@gmail.com>
2022-03-23 17:05:09 +0000

    Update abj-sql-psycopg2.pg

--- abj-sql-psycopg2.py
+++ abj-sql-psycopg2.py
@@ -8,7 +8,25 @@ connection = psycopg2.connect(database="chinook")
 cursor = connection.cursor()
 
 # Query 1 - select all records from the "Artist" table
-cursor.execute('SELECT * FROM "Artist"')
+# cursor.execute('SELECT * FROM "Artist"')
+
+# Query 2 - select only the "Name" column from the "Artist" table
+# cursor.execute('SELECT "Name" FROM "Artist"')
+
+# Query 3 - select only "Queen" from the "Artist" table
+# cursor.execute('SELECT * FROM "Artist" WHERE "Name" = %s', ["Queen"])
+
+# Query 4 - select only by "ArtistId" #51 from the "Artist" table
+# cursor.execute('SELECT * FROM "Artist" WHERE "ArtistId" = %s', [51])
+
+# Query 5 - select only the albums with "ArtistId" #51 on the "Album" table
+# cursor.execute('SELECT * FROM "Album" WHERE "ArtistId" = %s', [51])
+
+# Query 6 - select all tracks where the composer is "Queen" from the "Track" table
+# cursor.execute('SELECT * FROM "Track" WHERE "Composer" = %s', ["Queen"])
+
+# Query Test - select artist with name AC/DC
+cursor.execute('SELECT * FROM "Artist" WHERE "Name" = %s', ["AC/DC"])
 
 # fetch the results (multiple)
 results = cursor.fetchall()
